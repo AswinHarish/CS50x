25a310d7de51cc8469cb8142afc4ed954fded3b8
Aswin Harish <aswinhareesh03@gmail.com>
2023-05-24 20:45:11 +0530

    Mario Less

---
+++ Week 1, C/marioLess.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Declaring variables
+    int size, n = 1;
+
+    // Getting user input
+    do
+    {
+        do
+        {
+            size = get_int("Height: ");
+        }
+        while (size > 8);
+    }
+    while (size < 1);
+
+    // Printing pattern based on user input
+    for (int i = size; i >= 1; i--)
+    {
+        for (int j = 0; j < i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < n; k++)
+        {
+            printf("#");
+        }
+        // Printing new line
+        printf("\n");
+        n++;
+    }
+}
